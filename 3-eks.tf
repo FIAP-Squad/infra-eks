c18218f54c4d1084b7e98fbad2fe78d6b58314a7
Gabriel Silva <gabrielsilva.oliv20@gmail.com>
2024-04-25 21:07:45 -0300

    test: testing ruleset

--- 3-eks.tf
+++ 3-eks.tf
@@ -1,31 +1,7 @@
-resource "aws_iam_role" "eks-cluster" {
-  name = "${var.cluster_name}"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-cluster.name
-}
-
 resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   version  = var.cluster_version
-  role_arn = aws_iam_role.eks-cluster.arn
+  role_arn = "arn:aws:iam::941596849725:role/LabRole"
 
   vpc_config {
 
@@ -39,5 +15,4 @@ resource "aws_eks_cluster" "cluster" {
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
 }
--- 4-nodes.tf
+++ 4-nodes.tf
@@ -1,37 +1,7 @@
-resource "aws_iam_role" "eks_role" {
-  name = "eks_role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_role_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_role_AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_role_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_role.name
-}
-
 resource "aws_eks_node_group" "app" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "backend"
-  node_role_arn   = aws_iam_role.eks_role.arn
+  node_role_arn   = "arn:aws:iam::941596849725:role/LabRole"
   subnet_ids      = [
     aws_subnet.public-us-east-1a.id,
     aws_subnet.public-us-east-1b.id
@@ -43,9 +13,6 @@ resource "aws_eks_node_group" "app" {
     min_size     = 1
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_role_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks_role_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.eks_role_AmazonEC2ContainerRegistryReadOnly,
-  ]
 }
+
+## test
